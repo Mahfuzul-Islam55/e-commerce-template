b64739df26d9a0328363652550603ec30efa8905
Mahfuzul-Islam55 <shimulswe055@gmail.com>
2025-12-16 04:33:18 +0600

    feat: hero section design (in progress)

---
+++ app/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import styles from "@/app/components/HeroSection/HeroSection.module.css";
+
+const HeroSection = () => {
+  return (
+    <div>
+      <div style={{ position: "relative", width: "100%", aspectRatio: "16/9" }}>
+        <Image
+          src="/asset/heroImage.png"
+          alt=""
+          fill
+          style={{ objectFit: "contain", objectPosition: "top" }}
+        />
+
+        <div className={styles.backgroundImageOverlay}>
+          <div>
+            <p className={styles.heroTitle}>
+              Shop{" "}
+              <span style={{ color: "#15ADB7" }}>
+                Computer <br /> & experience
+              </span>
+            </p>
+            <p className={styles.heroSubtitle}>
+              You cannot inspect quality into the product; it is already
+              <br />
+              there.
+              <br /> I am not a product of my circumstances. I am a product of{" "}
+              <br /> my decisions.
+            </p>
+
+            <button className={styles.buttonContainer}>View More</button>
+          </div>
+
+          <div
+            style={{
+              marginLeft: "auto",
+              marginRight: "14.5vw",
+            }}
+          >
+            <Image
+              src="/asset/discountImage.png"
+              alt="Discount"
+              width={320}
+              height={320}
+              style={{
+                width: "clamp(50px, 12vw, 200px)",
+                height: "auto",
+                display: "block",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/app/components/Navbar/Navbar";
 import Sidenav from "@/app/components/Sidenav/Sidenav";
+import HeroSection from "@/app/components/HeroSection/HeroSection";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Sidenav />
+      <HeroSection />
     </>
   );
 }
