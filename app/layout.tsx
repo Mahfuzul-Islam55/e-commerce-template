9f1c2f15e073afa6981fcbdfbf2a9342ddb17bda
Mahfuzul-Islam55 <shimulswe055@gmail.com>
2025-12-15 18:49:13 +0600

    chore: initialize the project

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable}  `}>{children}</body>
+    </html>
+  );
+}
