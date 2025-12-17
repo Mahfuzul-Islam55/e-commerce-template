e7dc78da22bed113013bb7f22c1880fcb0359012
Mahfuzul-Islam55 <shimulswe055@gmail.com>
2025-12-17 06:44:39 +0600

    feat: fetching all products implementation

--- app/components/NewArrival/NewArrival.tsx
+++ app/components/NewArrival/NewArrival.tsx
@@ -1,6 +1,22 @@
 import styles from "@/app/components/NewArrival/NewArrival.module.css";
+import { getAllProducts } from "@/app/lib/api";
 import Image from "next/image";
-const NewArrival = () => {
+
+type product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+const NewArrival = async () => {
+  const products: product[] = await getAllProducts();
+
   return (
     <div className={styles.arrivalContainer}>
       <hr style={{ opacity: 0.17, marginBottom: "86.5px" }} />
@@ -9,25 +25,37 @@ const NewArrival = () => {
         <span style={{ color: "#00CAD7" }}>New</span> Arrivals
       </h3>
 
-      <div style={{ display: "flex", gap: "31px" }}>
-        <div className={styles.arrivalCardContainer}>
-          <h2 className={styles.categoryText}>Bin Bakar Electronics</h2>
-          <h3 className={styles.titleText}>Bin Bakar </h3>
-          <Image
-            src={"/asset/arrivalImage.png"}
-            alt=""
-            width={158}
-            height={129}
-          />
-          <div style={{ display: "flex", gap: "8px" }}>
-            <span className={styles.actualPriceText}>
-              <del>RS 110.000</del>
-            </span>
-            <span className={styles.discountPrice}>RS 56.000</span>
-          </div>
+      <div className={styles.newArrivalPageContainer}>
+        {Array.isArray(products) &&
+          products.map((data) => (
+            <div key={data.id} className={styles.arrivalCardContainer}>
+              <h2 className={styles.categoryText}>{data.category}</h2>
+              <h3 className={styles.titleText}>
+                {data.title.length > 40
+                  ? data.title.slice(0, 40) + ".."
+                  : data.title}{" "}
+              </h3>
 
-          <button className={styles.addToCartButton}>Add to cart</button>
-        </div>
+              <Image
+                style={{ marginTop: "auto" }}
+                src={`${data.image}`}
+                alt=""
+                width={158}
+                height={129}
+              />
+
+              <div style={{ display: "flex", gap: "8px", marginTop: "auto" }}>
+                <span className={styles.actualPriceText}>
+                  <del>
+                    RS {Math.floor(Math.random() * (200 - 100 + 1)) + 100}
+                  </del>
+                </span>
+                <span className={styles.discountPrice}>RS {data.price}</span>
+              </div>
+
+              <button className={styles.addToCartButton}>Add to cart</button>
+            </div>
+          ))}
       </div>
     </div>
   );
--- app/lib/api.ts
+++ app/lib/api.ts
@@ -9,3 +9,13 @@ export async function getProductsByCategory(category: string) {
   const data = await res.json();
   return data;
 }
+
+export async function getAllProducts() {
+  const res = await fetch(`https://fakestoreapi.com/products`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  const data = await res.json();
+  return data.length > 10 ? data.slice(0, 10) : data;
+}
